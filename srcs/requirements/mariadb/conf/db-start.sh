5da2f34759b4f43ba64e97068ba92a261d202bce
Jaime Martinez <jaimmart@c1r5s4.42madrid.com>
2024-06-11 17:10:33 +0200

    Adding comments to config files for better understanding.

--- srcs/requirements/mariadb/conf/db-start.sh
+++ srcs/requirements/mariadb/conf/db-start.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+# Define una variable que contiene la ruta del archivo SQL que se utilizará para inicializar la base de datos.
 MYSQL_INIT_FILE="/createdb.sql"
 
+# Cambia el propietario del directorio /var/lib/mysql y todos sus archivos y subdirectorios a mysql.
 chown -R mysql: /var/lib/mysql
 chmod 777 /var/lib/mysql
 
+# Inicializa los archivos de datos de MariaDB si aún no existen. Redirige la salida estándar y de error a /dev/null 
+# para suprimir cualquier mensaje.
 mysql_install_db >/dev/null 2>&1
 
+# Verifica si no existe un directorio con el nombre de la base de datos especificada. Si no existe, procede a crear e 
+# inicializar la base de datos y el usuario.
 if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    # Elimina el archivo de inicialización SQL si ya existe.
     rm -f "$MYSQL_INIT_FILE"
+    # echo ... >> "$MYSQL_INIT_FILE": Añade comandos SQL al archivo de inicialización para crear la base de datos, crear 
+    # un usuario con los permisos adecuados y establecer la contraseña del usuario root.
     echo "CREATE DATABASE $MYSQL_DATABASE;" >> "$MYSQL_INIT_FILE"
     echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "$MYSQL_INIT_FILE"
     echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> "$MYSQL_INIT_FILE"
     echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
     echo "FLUSH PRIVILEGES;" >> "$MYSQL_INIT_FILE"
     echo "Starting database..."
+    # Inicia el servidor MariaDB en modo seguro y ejecuta los comandos SQL contenidos en el archivo de inicialización. Redirige 
+    # la salida estándar y de error a /dev/null para suprimir cualquier mensaje.
     mysqld_safe --init-file=$MYSQL_INIT_FILE >/dev/null 2>&1
 else
     echo "Starting database..."
     mysqld_safe >/dev/null 2>&1
 fi
 
+# Si el directorio de la base de datos ya existe, inicia el servidor MariaDB en modo seguro. Redirige la salida estándar y de error a 
+# /dev/null para suprimir cualquier mensaje.
--- srcs/requirements/wordpress/conf/wp-start.sh
+++ srcs/requirements/wordpress/conf/wp-start.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 
+# Comprueba si el archivo wp-config.php ya existe. 
+# Esto se usa para determinar si WordPress ya está instalado.
 if [ -f ./wp-config.php ]
 then
     echo "Wordpress already installed."
 else
+    # Si wp-config.php no existe, descarga los archivos principales de WordPress.
     wp core download --allow-root
 
+    # Crea el archivo de configuración wp-config.php para WordPress.
     wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
 
+    # Instala WordPress con la URL, el título del sitio, el nombre de usuario del administrador, 
+    # la contraseña del administrador y el correo electrónico del administrador.
     wp core install --url=$DOMAIN_NAME --title="WordPress Site" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
 
+    # Crea un usuario adicional con el rol de editor.
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=editor --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
 fi
 
 echo "Starting Wordpress..."
+
+# Inicia PHP-FPM (FastCGI Process Manager) para manejar solicitudes PHP. 
+# El parámetro --nodaemonize asegura que PHP-FPM se ejecute en primer plano.
 /usr/sbin/php-fpm7.4 --nodaemonize
+
+# --allow-root: Permite que el comando se ejecute como usuario root, lo cual es útil cuando se ejecuta 
+#               dentro de contenedores Docker donde el script puede ejecutarse como root.
