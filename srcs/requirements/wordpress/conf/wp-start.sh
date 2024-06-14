f84aeac585aaa4387e4bfab4d14563baec6a96ed
Jaime Martinez <jaimmart@c1r2s5.42madrid.com>
2024-06-14 13:40:03 +0200

    Adding logs to wordpress script to ease mariadb availability monitoring

--- srcs/requirements/wordpress/conf/wp-start.sh
+++ srcs/requirements/wordpress/conf/wp-start.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+sleep 9
 # Verificar la disponibilidad de MariaDB
 while ! mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
     echo "Waiting for MariaDB..."
     sleep 2
 done
+
+echo "MariaDB is up and running"
 
 # Comprueba si el archivo wp-config.php ya existe. 
 # Esto se usa para determinar si WordPress ya está instalado.
@@ -12,16 +15,20 @@ if [ -f ./wp-config.php ]
 then
     echo "Wordpress already installed."
 else
+    echo "Downloading WordPress..."
     # Si wp-config.php no existe, descarga los archivos principales de WordPress.
     wp core download --allow-root
 
+    echo "Creating wp-config.php..."
     # Crea el archivo de configuración wp-config.php para WordPress.
     wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
 
+    echo "Installing WordPress..."
     # Instala WordPress con la URL, el título del sitio, el nombre de usuario del administrador, 
     # la contraseña del administrador y el correo electrónico del administrador.
     wp core install --url=$DOMAIN_NAME --title="WordPress Site" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
 
+    echo "Creating additional user..."
     # Crea un usuario adicional con el rol de editor.
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=editor --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
 fi
